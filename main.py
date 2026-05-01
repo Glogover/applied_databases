09f62722515a0577b46f6597ab8fda3d54468f87
Marcin Kaminski <marcin442228@gmail..com>
2026-05-01 19:02:26 +0100

    db connections

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ import mysql.connector # Importing the mysql.connector module to establish a con
 import dbconfig as cfg # Importing the dbconfig module to access the database configuration parameters defined in dbconfig.py
 from neo4j import GraphDatabase # Importing the GraphDatabase class from the neo4j module to establish a connection to the Neo4j database
 
+
+# DATABASE CONNECTIONS
+
 db = mysql.connector.connect(**cfg.mysql) # Unpacking the mysql dictionary from dbconfig.py to create a connection
 
 driver = GraphDatabase.driver(
     cfg.NEO4J_URI,
     auth=(cfg.NEO4J_USER, cfg.NEO4J_PASSWORD)
 ) # Creating a driver instance to connect to the Neo4j database using the URI and authentication credentials from dbconfig.py
+
+
+ 
+
+
 
 
 
